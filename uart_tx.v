1b42adc286959b02f898e7a8a4e704b5161305b9
Siddharth J <jsiddharth80@gmail.com>
2018-12-21 18:44:30 +0530

    Update uart_tx.v

--- uart_tx.v
+++ uart_tx.v
@@ -1,6 +1,23 @@
+/*
+ *  Copyright (C) 2018  Siddharth J <www.siddharth.pro>
+ *
+ *  Permission to use, copy, modify, and/or distribute this software for any
+ *  purpose with or without fee is hereby granted, provided that the above
+ *  copyright notice and this permission notice appear in all copies.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ *  WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ *  MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ *  ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ *  WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ *  ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ *  OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ */
+
 module uart_tx(clk,rst,start,tx_data_in,tx,tx_active,done_tx);
 
-parameter clk_freq = 32000000; //MHz
+parameter clk_freq = 50000000; //MHz
 parameter baud_rate = 19200; //bits per second
 input clk,rst;
 input start;
@@ -13,9 +30,9 @@ localparam clock_divide = (clk_freq/baud_rate);
 
 parameter [2:0] tx_IDLE = 3'b000,
                 tx_START = 3'b001,
-					 tx_DATA = 3'b010,
-					 tx_STOP = 3'b011,
-					 tx_DONE = 3'b100;
+		tx_DATA = 3'b010,
+	        tx_STOP = 3'b011,
+		tx_DONE = 3'b100;
 					 
 reg [2:0] tx_STATE, tx_NEXT;
 reg [11:0] clk_div_reg,clk_div_next;
